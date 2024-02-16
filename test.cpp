85f3365f48f55d8d10a1fefca81c2edbcf2c38b9
Francesco Sanfilippo <fr.sanfilippo@gmail.com>
2024-02-16 19:42:23 +0100

    Adding negation in the test

--- test.cpp
+++ test.cpp
@@ -4,9 +4,11 @@ constexpr void test()
 {
   // constexpr auto specs=createParserFromRegex("c|d(f?|g)","anna","[ciao]",".*");
   // constexpr auto parser=createParserFromRegex<specs>("c|d(f?|g)","anna","[ciao]",".*");
-  constexpr auto specs=createParserFromRegex("(\\+|\\-)?[0-9]+","(\\+|\\-)?[0-9]+(\\.[0-9]+)?((e|E)(\\+|\\-)?[0-9]+)?");
-  constexpr auto parser=createParserFromRegex<specs>("(\\+|\\-)?[0-9]+","(\\+|\\-)?[0-9]+(\\.[0-9]+)?((e|E)(\\+|\\-)?[0-9]+)?");
-  if(constexpr auto u=parser.parse("-332.235e-34"))
+  constexpr auto specs=createParserFromRegex("(\\+|\\-)?[0-9]+","(\\+|\\-)?[0-9]+(\\.[0-9]+)?((e|E)(\\+|\\-)?[0-9]+)?","[^h]");
+  constexpr auto parser=createParserFromRegex<specs>("(\\+|\\-)?[0-9]+","(\\+|\\-)?[0-9]+(\\.[0-9]+)?((e|E)(\\+|\\-)?[0-9]+)?","[^h]");
+  // if(constexpr auto u=parser.parse("-332.235e-34"))
+  //   printf("tok %zu\n",*u);
+  if(constexpr auto u=parser.parse("i"))
     printf("tok %zu\n",*u);
   
   // static_assert(parser.parse("3")==0);
