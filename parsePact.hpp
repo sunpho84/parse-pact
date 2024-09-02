1e308c53056893d6d203fd847151c8fb6eca49c8
Francesco Sanfilippo <fr.sanfilippo@gmail.com>
2024-09-03 01:33:42 +0200

    Added short licence, year, contact, url, include guard

--- parsePact.hpp
+++ parsePact.hpp
@@ -1,3 +1,27 @@
+#ifndef _PARSEPACT_HPP
+#define _PARSEPACT_HPP
+
+/*
+ * ParsePact - Provides Autonomous Regex Scanners & Entire Parsers At Compile Time
+ *
+ * Hosted at: https://github.com/sunpho84/parse-pact
+ *
+ * Copyright (C) 2024 Francesco Sanfilippo - fr.sanfilippo@gmail.com
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include <algorithm>
 #include <array>
 #include <cctype>
@@ -3724,3 +3748,5 @@ namespace pp
     return createGrammar<estimateGrammarSize(str)>(str);
   }
 }
+
+#endif
